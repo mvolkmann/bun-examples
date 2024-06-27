4bb6de60a70e5f76ae26a2c8ca6e10727e275b9d
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-06-26 21:26:26 -0500

    progress

--- bundle-demo/src/index.ts
+++ bundle-demo/src/index.ts
@@ -2,5 +2,6 @@ import {formatDistance} from 'date-fns';
 
 const now = new Date();
 const birthday = new Date(now.getFullYear(), 3, 16);
+if (birthday < now) birthday.setFullYear(now.getFullYear() + 1);
 const distance = formatDistance(now, birthday);
 console.log(distance, 'until your birthday');
